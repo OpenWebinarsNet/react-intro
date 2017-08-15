c0f20a032f5cc47041a93b0bf73233a164d84ac4
Miguh Ruiz <miguhruiz@gmail.com>
2017-08-15 19:25:00 +0200

    [Add] Functional Component

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import HelloWorldComponent from './components/HelloWorldComponent'
+
 class Application extends React.Component {
     render() {
         return(
-            <h1>Just config</h1>
+            <HelloWorldComponent />
         )
     }
 }
